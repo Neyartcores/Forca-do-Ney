b0a79b896a2ae61e887558656c91dc164f488b24
Neyartcores <artcorespersonalizacao@gmail.com>
2025-07-30 19:39:56 -0300

    Delete palavras.js

--- palavras.js
+++
@@ -1,5 +0,0 @@
-palavrasPorCategoria = {
-  'Frutas': ['banana', 'abacaxi', 'laranja', 'uva', 'melancia'],
-  'Animais': ['cachorro', 'gato', 'elefante', 'girafa', 'leão'],
-  'Países': ['brasil', 'canadá', 'japão', 'egito', 'alemanha']
-};
