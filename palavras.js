28ce7d98e023d062bb72a71cd9889e7cf07fb048
Neyartcores <artcorespersonalizacao@gmail.com>
2025-07-30 19:28:02 -0300

    Add files via upload

---
+++ palavras.js
@@ -0,0 +1,5 @@
+palavrasPorCategoria = {
+  'Frutas': ['banana', 'abacaxi', 'laranja', 'uva', 'melancia'],
+  'Animais': ['cachorro', 'gato', 'elefante', 'girafa', 'leão'],
+  'Países': ['brasil', 'canadá', 'japão', 'egito', 'alemanha']
+};
